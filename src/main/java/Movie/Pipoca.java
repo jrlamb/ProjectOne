ba1120a3c6508401f0c2fe57252ae3f4f0bf7c5a
marco <marco@LAPTOP-1037IREC>
2022-05-18 14:11:56 -0300

    Commit 6. Foi criada a classe pipoca.

---
+++ src/main/java/Movie/Pipoca.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Movie;
+
+/**
+ *
+ * @author marco
+ */
+public class Pipoca {
+    public void estourar(String[] args) {
+        System.out.println("Pow!!");
+    }
+}
