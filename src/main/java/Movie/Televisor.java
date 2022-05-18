9fd33ec255d7225dbf8b9d039c4073e786f8015b
gabriel <gabriel@DESKTOP-6EMC2SC>
2022-05-18 13:52:54 -0300

    Commit 3. Foi adicionada a classe televisor

---
+++ src/main/java/Movie/Televisor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Movie;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Televisor {
+    String marca;
+    
+    public void ligar(){
+        System.out.println("a tv foi ligada");
+    }
+    
+    public void desligar(){
+        System.out.println("a tv foi desligada");
+    }
+}
+
